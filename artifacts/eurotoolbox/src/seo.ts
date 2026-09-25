454d11ed73dade2ca12f0bac62f0764d68cb695e
ah5194851-coder <ah5194851@gmail.com>
2026-09-26 01:14:45 +0500

    Update seo.ts

--- artifacts/eurotoolbox/src/seo.ts
+++ artifacts/eurotoolbox/src/seo.ts
@@ -207,4 +207,6 @@ export function updateDocumentHead(path: string) {
   link.href = canonical;
 }
 
-
+function escapeHtml(value: string) {
+  return value.replace(/[&<>"']/g, character => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;' })[character] ?? character);
+}
